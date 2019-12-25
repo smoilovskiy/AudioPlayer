badbc9cc53f9a692f88df6a2204f29774da961d9
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2019-12-25 23:28:49 +0200

    Add new buttons

--- src/main/java/com/example/audioplayer/MainActivity.java
+++ src/main/java/com/example/audioplayer/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     SeekBar seekBar;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
